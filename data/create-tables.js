0975f038ef9eb2a3b659758d00638913e2525cf2
Franco Ortega <francopdx@gmail.com>
2020-11-02 18:12:19 -0800

    change file name and keys

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,12 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE potions (
                     id SERIAL PRIMARY KEY NOT NULL,
-                    name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    potion VARCHAR(512) NOT NULL,
+                    spell_level INTEGER NOT NULL,
+                    tasty BOOLEAN NOT NULL,
+                    brand  VARCHAR(512) NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const potions = require('./potions.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -25,12 +25,12 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      potions.map(potion => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
-                    VALUES ($1, $2, $3);
+                    INSERT INTO potions (potion, spell_level, owner_id)
+                    VALUES ($1, $2, $3, $4, $5);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+        [potion.potion, potion.spell_level, potion.tasty, potion.brand, user.id]);
       })
     );
     
