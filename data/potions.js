45ae402c89f957cc993487541ce58d93158e5bd8
Franco Ortega <francopdx@gmail.com>
2020-11-04 14:28:05 -0800

    add brands data file and update potions data file

--- data/potions.js
+++ data/potions.js
@@ -3,24 +3,24 @@ module.exports = [
     potion: 'heal',
     spell_level: 1,
     tasty: true,
-    brand: 'Ismelda\'s Elixir\'s'
+    brand_id: 'Ismelda\'s Elixir\'s'
   },
   {
     potion: 'sleep',
     spell_level: 3,
     tasty: false,
-    brand: 'Davan\'s Draughts'
+    brand_id: 'Davan\'s Draughts'
   },
   {
     potion: 'fly',
     spell_level: 5,
     tasty: true,
-    brand: 'Arkex Brews'
+    brand_id: 'Arkex Brews'
   },
   {
     potion: 'eagle eyes',
     spell_level: 2,
     tasty: false,
-    brand: 'Wild Tonics'
+    brand_id: 'Wild Tonics'
   }
 ];
