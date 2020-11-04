285760b650c9d8ca3fec0d456803e653c670c1e4
Franco Ortega <francopdx@gmail.com>
2020-11-03 17:54:47 -0800

    add tests for put and delete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -76,7 +76,6 @@ describe('app routes', () => {
       expect(data.body).toEqual(expectation);
     });
 
-
     test('returns a single potion', async() => {
       const expectation = {
         id: 1,
@@ -95,8 +94,7 @@ describe('app routes', () => {
       expect(data.body).toEqual(expectation);
     });
 
-
-    test.only('adds a single potion to the database and returns it', async() => {
+    test('adds a single potion to the database and returns it', async() => {
       const expectation = {
         id: 5,
         potion: 'swim',
@@ -127,7 +125,81 @@ describe('app routes', () => {
       expect(allPotions.body.length).toEqual(5);
     });
 
+    test.only('updates a single potion to the database and returns it', async() => {
+      const expectation = {
+        id: 1,
+        potion: 'swim faster',
+        spell_level: 75,
+        tasty: false,
+        brand: 'Water Brews',
+        owner_id: 1
+      };
 
+      const data = await fakeRequest(app)
+        .put('/potions/1')
+        .send({
+          potion: 'swim faster',
+          spell_level: 75,
+          tasty: false,
+          brand: 'Water Brews',
+          owner_id: 1
+        })
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('deletes a single potion from the database', async() => {
+      const expectation = [
+        {
+          id: 1,
+          potion: 'heal',
+          spell_level: 1,
+          tasty: true,
+          brand: 'Ismelda\'s Elixir\'s',
+          owner_id: 1
+        },
+        {
+          id: 2,
+          potion: 'sleep',
+          spell_level: 3,
+          tasty: false,
+          brand: 'Davan\'s Draughts',
+          owner_id: 1
+        },
+        {
+          id: 3,
+          potion: 'fly',
+          spell_level: 5,
+          tasty: true,
+          brand: 'Arkex Brews',
+          owner_id: 1
+        },
+      ];
+
+      const deletedItem =   {
+        id: 4,
+        potion: 'eagle eyes',
+        spell_level: 2,
+        tasty: false,
+        brand: 'Wild Tonics',
+        owner_id: 1
+      };
+
+      const data = await fakeRequest(app)
+        .delete('/potions/4')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const allPotions = await fakeRequest(app)
+        .get('/potions')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(deletedItem);
+      expect(allPotions.body).toEqual(expectation);
+    });
 
   });
   
