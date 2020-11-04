98544c4bf0eb3c0f40d5d4f3a4308dd58d5b8fda
Franco Ortega <francopdx@gmail.com>
2020-11-04 14:51:05 -0800

    add GET for all brands

--- data/potions.js
+++ data/potions.js
@@ -3,24 +3,24 @@ module.exports = [
     potion: 'heal',
     spell_level: 1,
     tasty: true,
-    brand_id: 'Ismelda\'s Elixir\'s'
+    brand_id: 1
   },
   {
     potion: 'sleep',
     spell_level: 3,
     tasty: false,
-    brand_id: 'Davan\'s Draughts'
+    brand_id: 2
   },
   {
     potion: 'fly',
     spell_level: 5,
     tasty: true,
-    brand_id: 'Arkex Brews'
+    brand_id: 3
   },
   {
     potion: 'eagle eyes',
     spell_level: 2,
     tasty: false,
-    brand_id: 'Wild Tonics'
+    brand_id: 4
   }
 ];
